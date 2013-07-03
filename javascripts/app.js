2607411b86e61ba54eafede00dfee55207939cd3
arunthampi <arun.thampi@gmail.com>
2013-07-03 00:39:08 +0000

    Add Chrome Webstore Inline install to App

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,6 +1,18 @@
-window.onscroll = function() {
-  document.getElementById('page-header').className = "active";
-  if(window.scrollY < 25) {
-    document.getElementById('page-header').className = "";
+$(document).ready(function($) {
+  if(window.chrome && window.chrome.webstore) {
+    if(window.chrome.app.isInstalled == false) {
+      var $div = $('<div>').attr('id', 'install-chrome-app').html("<p>Install Our Chrome App</p>");
+      $('body').prepend($div);
+
+      $div.click(function() {
+        var url = $('link[rel=chrome-webstore-item]').attr('href');
+
+        window.chrome.webstore.install(url, function(o) {
+          $('#install-chrome-app').remove();
+        }, function(error) {
+        });
+      });
+    }
   }
-};
+});
+
